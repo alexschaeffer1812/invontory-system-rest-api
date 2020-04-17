f2742d5130baf2fba7cfecb8cefc96f8b0d382a9
Alex Schaeffer <alex5638@icloud.com>
2020-04-17 16:26:24 -0600

    Updated port number

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.get("/", (req, res) => {
 const router = require('./routes/book.js')
 app.use(router)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
-// localhost:3000
+// localhost:5000
 app.listen(PORT, () => {
     console.log("Console is up and listening on " + PORT)
 })
